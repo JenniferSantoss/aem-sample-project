b6993040234f84381c2e9c97c05f6a0ac94e613b
Jennifer Santos <jennylima10@gmail.com>
2023-11-27 16:20:17 -0300

    Add sling model

--- sampleproject/core/src/main/java/br/com/mpa/sampleproject/core/models/FichaPessoal.java
+++ sampleproject/core/src/main/java/br/com/mpa/sampleproject/core/models/FichaPessoal.java
@@ -5,7 +5,7 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables = { SlingHttpServletRequest.class }, adapters = { MeuComponenteAem.class }, resourceType = {
+@Model(adaptables = { SlingHttpServletRequest.class }, adapters = { FichaPessoal.class }, resourceType = {
         FichaPessoal.RESOURCE_TYPE }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 
 public class FichaPessoal {
